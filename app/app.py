01147e8be6450e61bc7991c6e78028d7ba163666
felipeclarindo <fglpc2@gmail.com>
2024-09-27 07:26:25 -0300

    Iniciando cli

---
+++ app/app.py
@@ -0,0 +1,45 @@
+from os import name, system
+
+
+class App:
+    def __init__(self) -> None:
+        pass
+
+    def clear(self) -> None:
+        if name == "nt":
+            system("cls")
+        else:
+            system("clear")
+
+    def menu(self):
+        self.clear()
+        print("-" * 40)
+        print("--------- Task Manager --------")
+        print("-" * 40)
+        print("1 - Adicionar Tarefa.")
+        print("2 - Atualizar Tarefa.")
+        print("3 - Remover Tarefa.")
+        print("4 - Ver Tarefas")
+        print("5 - Sair")
+
+    def run(self):
+        sair = False
+        while not sair:
+            self.menu()
+            option = str(input("Informe uma opção: "))
+            match option:
+                case "1":
+                    pass
+                case "2":
+                    pass
+                case "3":
+                    pass
+                case "4":
+                    pass
+                case "5":
+                    saida_valida = False
+                    while not saida_valida:
+                        self.menu()
+
+                case _:
+                    print("Opção ínvalida.")
