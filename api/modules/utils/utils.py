38e526374c4f04e94facfab213550e6abba281f1
felipeclarindo <fglpc2@gmail.com>
2024-10-07 17:52:20 -0300

    Adicionando funções utilitarias

--- api/modules/utils/utils.py
+++ api/modules/utils/utils.py
@@ -6,22 +6,25 @@ class EstadoUser(StrEnum):
     LIBERADO = "liberado"
     BLOQUEADO = "bloqueado"
 
+
 class TaskState(StrEnum):
     CONCLUIDO = "concluido"
     PENDENTE = "pendente"
     EM_PROGRESSO = "em progresso"
 
 
-def check_user_state(validacoes:list) -> EstadoUser:
+def check_user_state(validacoes: list) -> EstadoUser:
     for validacao in validacoes:
         if not validacao:
             return EstadoUser.BLOQUEADO
     return EstadoUser.LIBERADO
 
+
 def date_today() -> str:
 
     data = date.today()
     return data.strftime("%Y/%m/%d")
+
 
 def due_date(dias: int, createdAt: str = None, update: bool = False) -> str:
     dias = int(dias)
@@ -31,3 +34,7 @@ def due_date(dias: int, createdAt: str = None, update: bool = False) -> str:
     else:
         data = datetime.strptime(createdAt, "%d/%m/%Y") + delta
     return data.strftime("%Y/%m/%d")
+
+
+def get_data_criacao(id: int) -> str:
+    command = "SELECT data_criacao FROM relatos"
