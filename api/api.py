7bf92701fd7f457c8f856bbf579eb0134087066f
felipeclarindo <fglpc2@gmail.com>
2024-10-07 17:52:48 -0300

    Realizando ajustes na api

--- api/api.py
+++ api/api.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from requests import request
 from json import dumps
 from .modules.database.models import Crud
 from pydantic import BaseModel
@@ -14,22 +13,27 @@ class PostTask(BaseModel):
     prioridade: str
     prazo: str
 
+
 class PutTask(BaseModel):
     id: int
     titulo: str
     prioridade: str
     prazo: str
 
+
 class PatchTask(BaseModel):
     id: int
     dado: str
     novo_dado: str
 
+
 class DeleteTask(BaseModel):
     id: int
 
+
 class GetTaskWithId(BaseModel):
     id: int
+
 
 # Criando rota principal
 @app.get("/")
@@ -38,7 +42,7 @@ def index():
 
 
 # Criando rota de envio de dados
-@app.post("/tasks/post")
+@app.post("/tasks/post", METHODS=["POST"])
 def post_task(data: PostTask):
     response = crud.post(data.titulo, data.prioridade, data.prazo)
     if response["status"] == "succes":
@@ -48,7 +52,7 @@ def post_task(data: PostTask):
 
 
 # Criando rota de atualização de dados
-@app.put("/task/put")
+@app.put("/tasks/put", METHODS=["PUT"])
 def put_task(data: PutTask):
     response = crud.put(data.id, data.titulo, data.prioridade, data.prazo)
     if response["status"] == "success":
@@ -56,8 +60,9 @@ def put_task(data: PutTask):
     else:
         return dumps({"message": response["message"]})
 
+
 # Criando rota de atualização de um unico dado
-@app.patch("/task/patch")
+@app.patch("/tasks/patch", METHODS=["PATCH"])
 def patch_task(data: PatchTask):
     response = crud.patch(data.id, data.dado, data.novo_dado)
     if response["status"] == "succes":
@@ -67,17 +72,17 @@ def patch_task(data: PatchTask):
 
 
 # Criando rota de removação de dado
-@app.delete("/task/delete")
+@app.delete("/tasks/delete", METHODS=["DELETE"])
 def delete_task(data: DeleteTask):
-    deleted = crud.delete(data.id)
-    if deleted["status"] == "success":
+    response = crud.delete(data.id)
+    if response["status"] == "success":
         return dumps({"message": "Registro deletado com sucesso!"})
     else:
         return dumps({"message": response["message"]})
 
 
 # Criando rota de pegar dados
-@app.get("/task/get")
+@app.get("/tasks/get", METHODS=["GET"])
 def get_tasks():
     response = crud.get()
     if response["status"] == "success":
@@ -87,8 +92,8 @@ def get_tasks():
 
 
 # Criando rota de pegar dado com id
-@app.get("/tasks/get-with-id")
-def get_tasks_with_id(data: GetTaskWithId):
+@app.get("/tasks/get-with-id", METHODS=["GET"])
+def get_task_with_id(data: GetTaskWithId):
     response = crud.get_with_id(data.id)
     if response["status"] == "succes":
         return dumps({"message": response["message"]})
