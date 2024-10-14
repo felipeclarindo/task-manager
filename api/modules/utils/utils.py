68f9d6eccf603a3fdc78d7a2fdbcf56e2aec3246
felipeclarindo <fglpc2@gmail.com>
2024-10-13 22:19:52 -0300

    Adicionando funções utilitarias

--- api/modules/utils/utils.py
+++ api/modules/utils/utils.py
@@ -26,15 +26,32 @@ def date_today() -> str:
     return data.strftime("%Y/%m/%d")
 
 
-def due_date(dias: int, createdAt: str = None, update: bool = False) -> str:
+def due_date(dias: int, createdAt: datetime = None, update: bool = False) -> str:
     dias = int(dias)
     delta = timedelta(days=dias)
+
     if not update:
-        data = date.today() + delta
+        data = datetime.now() + delta
     else:
-        data = datetime.strptime(createdAt, "%d/%m/%Y") + delta
+        if createdAt is None:
+            raise ValueError("createdAt não pode ser None quando update é True.")
+        data = createdAt + delta
+
     return data.strftime("%Y/%m/%d")
 
 
-def get_data_criacao(id: int) -> str:
-    command = "SELECT data_criacao FROM relatos"
+def get_data_criacao(cursor, id: int) -> datetime:
+    command = "SELECT data_criacao FROM tarefas WHERE id = :id"
+    cursor.execute(command, {"id": id})
+    data_vencimento = cursor.fetchone()
+
+    if data_vencimento is not None:
+        data_vencimento = data_vencimento[0]
+        return data_vencimento
+    raise ValueError(f"Nenhuma data de criação encontrada para o ID: {id}")
+
+
+def convert_data(data: str) -> str:
+    print("convert_data")
+    data = datetime.strptime(data, "%Y-%m-%d %H:%M:%S")
+    return data.strftime("%d/%m/%Y")
