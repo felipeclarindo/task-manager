217989504bb4d44f2db3a710ab021bb7111c1e56
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-10-21 14:19:12 -0300

    Voltando o finally fechando o servidor smtp

--- api/modules/notification/notification.py
+++ api/modules/notification/notification.py
@@ -26,6 +26,7 @@ class Notificador:
             server.sendmail(msg['From'], to_email, msg.as_string())
 
             print("Email enviado com sucesso!")
-            server.quit()
         except Exception as e:
             print(f"Erro: {str(e)}")
+        finally:
+            server.quit()
